cd8992d4061ca35ffce43fbc5f9474bbe4460614
Robert Biggs <rbiggs@ymail.com>
2019-05-20 07:11:58 -0700

    Added dist versions.

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import minify from 'rollup-plugin-babel-minify'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/router.js',
+      format: 'umd',
+      name: 'composi',
+      sourcemap: true,
+      sourcemapFile: 'dist/router.js.map'
+    },
+    {
+      file: 'dist/router.mjs',
+      format: 'esm',
+      name: 'composi',
+      sourcemap: true,
+      sourcemapFile: 'dist/router.mjs.map'
+    }
+  ],
+  plugins:
+    [
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      resolve({
+        jsnext: true,
+        main: true,
+        browser: true
+      }),
+      commonjs(),
+      minify({
+        mangle: { topLevel: true },
+        comments: false
+      })
+    ]
+}
